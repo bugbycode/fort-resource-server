ecfc1b27b81a3977ffffd36a3d1d7872073a521b
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2019-01-17 17:52:10 +0800

    更新数据库

--- src/main/resources/new_resource.sql
+++ src/main/resources/new_resource.sql
@@ -31,15 +31,11 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `account` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '账号',
   `password` text COLLATE utf8_unicode_ci COMMENT '密码',
-  `type` tinyint(4) DEFAULT '0' COMMENT '账号类型',
-  `resource_id` int(11) DEFAULT NULL COMMENT '资产ID',
-  `server_type` int(11) DEFAULT '0' COMMENT '服务类型',
-  PRIMARY KEY (`id`),
-  KEY `resource_id` (`resource_id`),
-  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`resource_id`) REFERENCES `resource` (`id`) ON DELETE CASCADE
+  `type` tinyint(4) DEFAULT '0' COMMENT '账号类型（0：管理员账号，1：普通账号）',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -83,6 +79,32 @@ INSERT INTO `network` VALUES (1,'本地局域网',0,NULL,NULL,NULL,'2019-01-16 1
 UNLOCK TABLES;
 
 --
+-- Table structure for table `rel_server_account`
+--
+
+DROP TABLE IF EXISTS `rel_server_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rel_server_account` (
+  `server_id` int(11) DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  KEY `server_id` (`server_id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `rel_server_account_ibfk_1` FOREIGN KEY (`server_id`) REFERENCES `resource_server` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `rel_server_account_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rel_server_account`
+--
+
+LOCK TABLES `rel_server_account` WRITE;
+/*!40000 ALTER TABLE `rel_server_account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rel_server_account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `resource`
 --
 
@@ -96,10 +118,6 @@ CREATE TABLE `resource` (
   `status` tinyint(4) DEFAULT NULL COMMENT '状态',
   `type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `os_type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `use_ssh` tinyint(4) DEFAULT '0' COMMENT '是否开启SSH协议',
-  `ssh_port` int(11) DEFAULT '22' COMMENT 'SSH协议端口号',
-  `use_rdp` tinyint(4) DEFAULT '0',
-  `rdp_port` int(11) DEFAULT '3389',
   `network_id` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '修改时间',
@@ -115,8 +133,37 @@ CREATE TABLE `resource` (
 
 LOCK TABLES `resource` WRITE;
 /*!40000 ALTER TABLE `resource` DISABLE KEYS */;
-INSERT INTO `resource` VALUES (1,'计算机','dev.server',1,'Windows','Windows 7',0,22,0,3389,1,'2019-01-16 16:17:00',NULL);
+INSERT INTO `resource` VALUES (1,'计算机','dev.server',1,'Windows','Windows 7',1,'2019-01-16 16:17:00',NULL);
 /*!40000 ALTER TABLE `resource` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resource_server`
+--
+
+DROP TABLE IF EXISTS `resource_server`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resource_server` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `server_type` int(11) DEFAULT '0' COMMENT '服务类型（0：ssh或sftp，1：远程桌面，2：telnet，3：oracle，4、sqlserver，5：ftp）',
+  `port` int(11) DEFAULT '22' COMMENT '端口号',
+  `db_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '数据库名称',
+  `res_id` int(11) DEFAULT NULL COMMENT '所属资产ID',
+  `use` tinyint(4) DEFAULT '0' COMMENT '是否启用服务',
+  PRIMARY KEY (`id`),
+  KEY `res_id` (`res_id`),
+  CONSTRAINT `resource_server_ibfk_1` FOREIGN KEY (`res_id`) REFERENCES `resource` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resource_server`
+--
+
+LOCK TABLES `resource_server` WRITE;
+/*!40000 ALTER TABLE `resource_server` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resource_server` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -128,4 +175,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-16 18:42:40
+-- Dump completed on 2019-01-17 17:51:38
